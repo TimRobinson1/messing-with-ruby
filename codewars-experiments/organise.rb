8e88b3f88dccb8afc7afdf45321e8a61fc8b6989
Timothy Robinson <timmyrobby@gmail.com>
2017-01-06 04:31:23 +0000

    Updated organise kata

--- codewars-experiments/organise.rb
+++ codewars-experiments/organise.rb
@@ -1,3 +1,5 @@
 def organise(str)
   return (str.split /(?=[A-Z])/).join(" ").strip.gsub(" ", "-").downcase.gsub(/[0-9]/, "").gsub(/^-+/,'')
 end
+
+# An example input is "MyCamelIsBig" which will produce "my-camel-is-big"
