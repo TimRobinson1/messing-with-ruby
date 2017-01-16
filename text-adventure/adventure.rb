fd1229076d7f96327167062f60baf9ac0547e75c
Timothy Robinson <timmyrobby@gmail.com>
2017-01-16 17:15:02 +0000

    Starting point for longer text adventure

---
+++ text-adventure/adventure.rb
@@ -0,0 +1,35 @@
+# A bare starting point for a text-based adventure.
+
+def inventory
+  puts "Current inventory:"
+end
+
+def north_route
+  puts "Going North"
+end
+
+def east_route
+  puts "Going East"
+end
+
+def help
+  puts "General commands:"
+  puts "1. 'check inventory' - Displays current inventory."
+end
+
+puts "You awaken in a dark wood. A winding path stretches in front of you
+to the North. There is also a darker path leading East. What do you do?"
+
+input = gets.chomp.downcase
+
+if input.include?("north")
+  north_route
+elsif input.include?("east")
+  east_route
+elsif input.include?("inventory")
+  inventory
+elsif input.include?("help")
+  help
+else
+  puts "What?"
+end
