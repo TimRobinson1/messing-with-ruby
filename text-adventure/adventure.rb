f92363e14c5460710dd32883e2612a85a0b495bd
Timothy Robinson <timmyrobby@gmail.com>
2017-01-17 16:23:20 +0000

    Added fight function

--- text-adventure/adventure.rb
+++ text-adventure/adventure.rb
@@ -10,8 +10,28 @@ def inventory
   puts "Current inventory: #{inventory}"
 end
 
+def response
+  loop do
+    input = gets.chomp.downcase
+    if input.split(" ").any? { |x| ["fight", "kill", "attack", "punch", "hit", "murder", "stab"].include? x }
+      puts "You fought the troll and won!"
+      exit
+    elsif input.split(" ").any? { |x| ["run", "flee", "go back", "leave", "return"].include? x }
+      puts "You attempt to reutrn to the previous area, but that hasn't been implemented yet."
+    elsif input.include?("inventory")
+      inventory
+    elsif input.include?("help")
+      help
+    else
+      puts "What?"
+    end
+  end
+end
+
 def north_route
-  puts "Going North"
+  puts "You head north and a troll stands in front of you.
+  He appears to want to test the fight function."
+  response
 end
 
 def east_route
@@ -21,22 +41,27 @@ end
 def help
   puts "General commands:"
   puts "1. 'check inventory' - Displays current inventory."
+  puts "2. 'flee' or 'run' - Returns player to previous area if possible."
 end
 
 puts "You awaken in a dark wood. A winding path stretches in front of you
 to the North. There is also a darker path leading East. What do you do?"
 
-loop do
-  input = gets.chomp.downcase
-  if input.include?("north")
-    north_route
-  elsif input.include?("east")
-    east_route
-  elsif input.include?("inventory")
-    inventory
-  elsif input.include?("help")
-    help
-  else
-    puts "What?"
+def start_woods
+  loop do
+    input = gets.chomp.downcase
+    if input.include?("north")
+      north_route
+    elsif input.include?("east")
+      east_route
+    elsif input.include?("inventory")
+      inventory
+    elsif input.include?("help")
+      help
+    else
+      puts "What?"
+    end
   end
 end
+
+start_woods
