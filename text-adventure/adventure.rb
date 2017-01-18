f31f6a5be94ee98409fe9529b28f0379586c0783
Timothy Robinson <timmyrobby@gmail.com>
2017-01-18 17:42:44 +0000

    Added health and preparing for save system

--- text-adventure/adventure.rb
+++ text-adventure/adventure.rb
@@ -1,6 +1,10 @@
 # A bare starting point for a text-based adventure.
 $inventory = ""
 
+def fight_mode(input)
+  input.split(" ").any? { |x| ["fight", "kill", "attack", "punch", "hit", "murder", "stab"].include? x }
+end
+
 def inventory
   if $inventory.empty?
     inventory = "nothing"
@@ -13,11 +17,13 @@ end
 def response
   loop do
     input = gets.chomp.downcase
-    if input.split(" ").any? { |x| ["fight", "kill", "attack", "punch", "hit", "murder", "stab"].include? x }
+    if fight_mode(input)
       puts "You fought the troll and won!"
       exit
     elsif input.split(" ").any? { |x| ["run", "flee", "go back", "leave", "return"].include? x }
-      puts "You attempt to reutrn to the previous area, but that hasn't been implemented yet."
+      puts "You run back to the previous area."
+      a = caller[0][/`([^']*)'/, 1]
+      puts caller[a]
     elsif input.include?("inventory")
       inventory
     elsif input.include?("help")
@@ -42,13 +48,14 @@ def help
   puts "General commands:"
   puts "1. 'check inventory' - Displays current inventory."
   puts "2. 'flee' or 'run' - Returns player to previous area if possible."
+  puts "3. 'save game' - Saves progress to 'saves.csv'"
+  puts "4. 'load game' - Loads progress from 'saves.csv'"
 end
 
-puts "You awaken in a dark wood. A winding path stretches in front of you
-to the North. There is also a darker path leading East. What do you do?"
-
 def start_woods
-  loop do
+  puts "You awaken in a dark wood. A winding path stretches in front of you
+  to the North. There is also a darker path leading East. What do you do?"
+  while true do
     input = gets.chomp.downcase
     if input.include?("north")
       north_route
