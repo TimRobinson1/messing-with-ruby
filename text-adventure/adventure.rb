9e46755f551b4a2c55b9727f215e92e4b791e52d
Timothy Robinson <timmyrobby@gmail.com>
2017-01-18 18:15:09 +0000

    Added health and emotions feature

--- text-adventure/adventure.rb
+++ text-adventure/adventure.rb
@@ -1,11 +1,19 @@
 # A bare starting point for a text-based adventure.
+
+# Starting character statistics
 $inventory = ""
+$health = 100
+$feeling = "a bit confused"
 
 def fight_mode(input)
   input.split(" ").any? { |x| ["fight", "kill", "attack", "punch", "hit", "murder", "stab"].include? x }
 end
 
-def inventory
+def flee_mode(input)
+  input.split(" ").any? { |x| ["flee", "run", "retreat", "fall back", "hide"].include? x }
+end
+
+def get_inventory
   if $inventory.empty?
     inventory = "nothing"
   else
@@ -14,30 +22,36 @@ def inventory
   puts "Current inventory: #{inventory}"
 end
 
-def response
-  loop do
-    input = gets.chomp.downcase
-    if fight_mode(input)
-      puts "You fought the troll and won!"
-      exit
-    elsif input.split(" ").any? { |x| ["run", "flee", "go back", "leave", "return"].include? x }
-      puts "You run back to the previous area."
-      a = caller[0][/`([^']*)'/, 1]
-      puts caller[a]
-    elsif input.include?("inventory")
-      inventory
-    elsif input.include?("help")
+def generic_responses
+    if $input.include?("inventory")
+      get_inventory
+    elsif $input.split(" ").any? { |x| ["check health", "show health", "display health"].include? x }
+      puts "Your current health is: #{$health}"
+    elsif $input.include?("help")
       help
+    elsif $input.split(" ").any? { |x| ["show feelings", "check feelings", "show feeling", "check feeling", "feelings"].include? x }
+      puts "Currently, you are feeling #{$feeling}."
     else
-      puts "What?"
+      puts "You're unsure what to do."
     end
-  end
 end
 
 def north_route
   puts "You head north and a troll stands in front of you.
   He appears to want to test the fight function."
-  response
+  loop do
+    input = gets.chomp.downcase
+    if fight_mode(input)
+      puts "You beat the troll!"
+      exit(0)
+    elsif flee_mode(input)
+      puts "You flee back to the previous area!"
+      start_woods
+    else
+      $input = input
+      generic_responses
+    end
+  end
 end
 
 def east_route
@@ -47,13 +61,14 @@ end
 def help
   puts "General commands:"
   puts "1. 'check inventory' - Displays current inventory."
-  puts "2. 'flee' or 'run' - Returns player to previous area if possible."
+  puts "2. 'check health' - Displays current health level."
+  puts "3. 'show feelings' - Displays characater's current emotional state."
   puts "3. 'save game' - Saves progress to 'saves.csv'"
   puts "4. 'load game' - Loads progress from 'saves.csv'"
 end
 
 def start_woods
-  puts "You awaken in a dark wood. A winding path stretches in front of you
+  puts "You find yourself in a dark wood. A winding path stretches in front of you
   to the North. There is also a darker path leading East. What do you do?"
   while true do
     input = gets.chomp.downcase
@@ -66,9 +81,10 @@ def start_woods
     elsif input.include?("help")
       help
     else
-      puts "What?"
+      puts "You stumble around aimlessly"
     end
   end
 end
 
+puts "With a sharp pain in your head, you awaken."
 start_woods
