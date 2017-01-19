3c3adef41bc6749f64e95911c7b7f35261e5bd71
Timothy Robinson <timmyrobby@gmail.com>
2017-01-19 17:48:19 +0000

    Replaced input with global variable

--- text-adventure/adventure.rb
+++ text-adventure/adventure.rb
@@ -6,16 +6,16 @@ $has_weapon = false
 $health = 100
 $feeling = "a bit confused"
 
-def fight_mode(input)
-  input.split(" ").any? { |x| ["fight", "kill", "attack", "punch", "hit", "murder", "stab"].include? x }
+def fight_mode($input)
+  $input.split(" ").any? { |x| ["fight", "kill", "attack", "punch", "hit", "murder", "stab"].include? x }
 end
 
-def flee_mode(input)
-  input.split(" ").any? { |x| ["flee", "run", "retreat", "fall back", "hide"].include? x }
+def flee_mode($input)
+  $input.split(" ").any? { |x| ["flee", "run", "retreat", "fall back", "hide"].include? x }
 end
 
-def talk_mode(input)
-  input.split(" ").any? { |x| ["talk", "chat", "converse", "speak"].include? x }
+def talk_mode($input)
+  $input.split(" ").any? { |x| ["talk", "chat", "converse", "speak"].include? x }
 end
 
 def get_inventory
@@ -53,21 +53,20 @@ def north_route
   puts "You head north and a troll stands in front of you.\nHe appears to want to test the fight function."
   $feeling = "a bit scared"
   loop do
-    input = gets.chomp.downcase
-    if fight_mode(input) && $has_weapon
+    $input = gets.chomp.downcase
+    if fight_mode($input) && $has_weapon
       puts "You beat the troll!"
       exit(0)
-    elsif fight_mode(input)
+    elsif fight_mode($input)
       puts "You try to fight the troll, but you're unarmed.\nThe troll makes quick work of you."
       dead
-    elsif flee_mode(input)
+    elsif flee_mode($input)
       puts "You flee back to the previous area!"
       $feeling = "a bit shaken up"
       start_woods
-    elsif talk_mode(input)
+    elsif talk_mode($input)
       puts "You try to communicate, but the Troll only\nunderstands the Trollian language."
     else
-      $input = input
       generic_responses
     end
   end
@@ -77,15 +76,14 @@ def east_route
   puts "You decide to head east and come upon a small village."
   puts "From where you're standing, you can see a shop,\na tavern and a town hall. What do you do?"
   loop do
-    input = gets.chomp.downcase
-    if input.include?("tavern")
+    $input = gets.chomp.downcase
+    if $input.include?("tavern")
       puts "Heading to tavern!"
-    elsif input.include?("shop")
+    elsif $input.include?("shop")
       puts "Heading to the shop!"
-    elsif input.include?("hall")
+    elsif $input.include?("hall")
       puts "Heading to town hall!"
     else
-      $input = input
       generic_responses
     end
   end
@@ -103,17 +101,16 @@ end
 def start_woods
   puts "You find yourself in a dark wood. A winding path stretches in front of you\nto the North. There is also a darker path leading East. What do you do?"
   while true do
-    input = gets.chomp.downcase
-    if input.include?("north")
+    $input = gets.chomp.downcase
+    if $input.include?("north")
       north_route
-    elsif input.include?("east")
+    elsif $input.include?("east")
       east_route
-    elsif input.include?("inventory")
+    elsif $input.include?("inventory")
       inventory
-    elsif input.include?("help")
+    elsif $input.include?("help")
       help
     else
-      $input = input
       generic_responses
     end
   end
