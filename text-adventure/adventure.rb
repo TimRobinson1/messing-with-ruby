8c250258e3cb26b22a0de35f859fc2ba3dbbf665
Timothy Robinson <timmyrobby@gmail.com>
2017-02-02 21:27:57 +0000

    Added save function

--- text-adventure/adventure.rb
+++ text-adventure/adventure.rb
@@ -1,16 +1,17 @@
-# A bare starting point for a text-based adventure.
+require 'CSV'
 
 # Starting character statistics
 $inventory = ""
 $has_weapon = false
 $health = 100
 $feeling = "a bit confused"
+$location = "start_woods"
 
 class Defeat
   def initialize(enemy)
     types = ["vicious", "monstrous", "murderous", "very evil", "powerful"]
     version = types[rand(0..4)]
-    messages = ["That #{version} #{enemy} made very quick work of you!", "That's not good, that #{enemy} was #{version}. Too bad for you.", "You really thought you could beat that #{version} #{enemy}?", "Ouch, that #{enemy} pulverized you.", "Dead! Hopefully the #{enemy} will make funeral arrangements too."]
+    messages = ["That #{version} #{enemy} made very quick work of you!", "Whoops, turns out that #{enemy} was #{version}. Too bad for you.", "You really thought you could beat that #{version} #{enemy}?", "Ouch, that #{enemy} pulverized you.", "Dead! Hopefully the #{enemy} will make funeral arrangements too."]
     puts messages[rand(0..4)]
     puts "Game over!"
   end
@@ -21,7 +22,7 @@ def fight_mode(input)
 end
 
 def flee_mode(input)
-  input.split(" ").any? { |x| ["flee", "run", "retreat", "fall back", "hide"].include? x }
+  input.split(" ").any? { |x| ["flee", "run", "retreat", "hide"].include? x }
 end
 
 def talk_mode(input)
@@ -37,15 +38,25 @@ def get_inventory
   puts "Current inventory: #{inventory}"
 end
 
+def save_game
+  CSV.open("saves.csv", "wb") do |csv|
+    csv << [$inventory, $has_weapon, $health, $location]
+  end
+  puts "Game saved successfully!"
+end
+
 def generic_responses
     if $input.include?("inventory")
       get_inventory
-    elsif $input.split(" ").any? { |x| ["health", "check health", "show health", "display health"].include? x }
+    elsif $input.split(" ").any? { |x| ["health", "life", "hitpoints"].include? x }
       puts "Your current health is: #{$health}"
     elsif $input.include?("help")
       help
-    elsif $input.split(" ").any? { |x| ["show feelings", "check feelings", "show feeling", "check feeling", "feelings"].include? x }
+    elsif $input.split(" ").any? { |x| ["feelings", "emotions", "feeling", "emotion"].include? x }
       puts "Currently, you are feeling #{$feeling}."
+    elsif $input == "save game"
+      puts "Saving the game..."
+      save_game
     else
       puts "You're unsure what to do."
     end
@@ -79,6 +90,7 @@ end
 def east_route
   puts "You decide to head east and come upon a small village."
   puts "From where you're standing, you can see a shop,\na tavern and a town hall. What do you do?"
+  $feeling = "fine"
   loop do
     input = gets.chomp.downcase
     if input.include?("tavern")
@@ -133,4 +145,4 @@ def start_woods
 end
 
 puts "With a sharp pain in your head, you awaken."
-start_woods
+send("#{$location}")
