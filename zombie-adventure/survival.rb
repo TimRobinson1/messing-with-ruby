fc368050f56218f03d9dd1273d254c40ba40e911
Timothy Robinson <timmyrobby@gmail.com>
2017-03-04 00:48:35 +0000

    Start of user input

--- zombie-adventure/survival.rb
+++ zombie-adventure/survival.rb
@@ -52,6 +52,43 @@ player = Player.new
 
 map = Map.new
 
-map.visit 2
+puts "Nobody saw the zombie apocalypse coming, yet here it is..."
+puts "You managed to survive the initial outbreak."
+puts "You find yourself in a small house with two other survivors."
+puts "Would you like to name these survivors?"
 
-map.show
+input = gets.chomp.downcase
+if input == "yes"
+  print "Survivor 1: "
+  name = gets.chomp
+  survivors.push(name)
+  print "Survivor 2: "
+  name = gets.chomp
+  survivors.push(name)
+else
+  puts "Of course not, they must already have names!"
+  2.times do
+    survivors.push(["Amelia", "Andrei", "Joel", "Louis", "Bill", "Zoey", "Francis", "Ellie", "Sarah", "Kim"].sample)
+  end
+end
+
+puts "*"*30
+puts "A new day dawns.  Day 1."
+puts "You have 10 portions of daily food."
+puts "The water supply is still functional."
+puts "You've set up your base in the small house."
+puts "Through the window you can check on the zombie crowds."
+puts "The other survivors look to you for guidance."
+puts "What do you want to do?"
+
+input = gets.chomp.downcase
+
+show_map = ["show map", "check map", "map", "open map", "display map"]
+
+if show_map.include?(input)
+  map.show
+elsif input == "help"
+  puts "helping!"
+elsif input == "check survivor status"
+  puts survivors
+end
